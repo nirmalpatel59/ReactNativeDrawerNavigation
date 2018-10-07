b0f36b76aedf1011b77b7216b38c4abc7284d4a0
Nirmal Patel <nirmal.patel.59@gmail.com>
2018-10-07 10:34:01 +0530

    yellowBox iserted to avoid warnings

--- App.js
+++ App.js
@@ -5,29 +5,21 @@
  */
 
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View
-} from 'react-native';
 import DrawerNavigator from './src/screens';
 
 export default class App extends Component {
   render() {
     return (
-      <DrawerNavigator>
-        <View style={styles.container}>
-            This is Home page    
-        </View>
-      </DrawerNavigator>
+      <DrawerNavigator />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   }
+// });
